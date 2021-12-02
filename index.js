42e45c7fb31aff8c81b99cbca0fcaec289de16ed
Julia <julia.maliutsina@gmail.com>
2021-12-02 16:38:02 +0300

    task2

--- index.js
+++ index.js
@@ -12,6 +12,35 @@ app.get('/api/greetings', (request, response) => {
   : response.status(400).json({ error: 'Name undefined' })
 })
 
+app.route('/api/notes')
+.get((request, response) => {
+  response.send([2]);
+})
+.post((request, response) => {
+  response.send(request.body);
+})
+
+app.route('/api/notes/:id')
+.put((request, response) => {
+  let noteId = request.params.id;
+  let putNoteResponse = {
+    id: noteId,
+    title: request.body.title,
+    content: request.body.content,
+    createdAt: request.body.content,
+    updatedAt: request.body.updatedAt,    
+  };
+  response.send(putNoteResponse);
+})
+.delete((request, response) => {
+  let noteId = request.params.id;
+  let deleteNoteResponse = {
+    success: true, 
+    id: noteId,   
+  };
+  response.send(deleteNoteResponse);
+}) 
+
 app.listen(PORT, () => {
   console.log(`Server has been started on port ${PORT} ...`)
 });
