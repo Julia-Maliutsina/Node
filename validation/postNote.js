329d98d527da8742a22576548bd79d7d2d6a4756
Julia <julia.maliutsina@gmail.com>
2021-12-03 15:20:50 +0300

    task 3 corrected

--- validation/postNote.js
+++ validation/postNote.js
@@ -3,7 +3,7 @@ import Joi from "joi";
 export const validateNewNote = (note) => {
   const requirements = Joi.object().keys({
     id: Joi.number().required(),
-    title: Joi.string().alphanum().min(3).required(),
+    title: Joi.string().min(3).required(),
     content: Joi.string().min(3).max(500).required(),
     createdAt: Joi.string().required(),
     updatedAt: Joi.string(),
